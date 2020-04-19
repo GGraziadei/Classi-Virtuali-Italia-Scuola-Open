7e79968d56b1c85aba97616cd281a50578edf366
Gianluca <graziadei.gianluca@gmail.com>
2020-04-19 03:29:35 +0200

    Miglioramenti grafici

--- school/accesso.php
+++ school/accesso.php
@@ -22,7 +22,7 @@
     <tr>
       <th>Ora di accesso</th>
       <th>Alunno</th>
-      <th>Link Classe Virtuale</th>
+      <th>Classe Virtuale</th>
      
     </tr>
   </thead>
@@ -30,7 +30,8 @@
     <tr>
       <td data-column="First Name"><?php echo $ora;?></td>
       <td data-column="First Name"><?php echo $_ENV["USERNAME"]?></td>
-      <td data-column="Last Name"><a href="javascript:window.open('<?php echo $link;?>', '', 'width=1024,height=720')">CLICCA QUI PER ACCEDERE</a></td>
+      <td data-column="Last Name"><button class = 'button' onclick="openWin()">Accedi </button>
+      <button  class = 'button' onclick="closeWin()">Esci</button></td>
 
       
     </tr>
@@ -38,13 +39,38 @@
   </tbody>
 </table>
 
- 
+<button  class = 'button' onclick="javascript:window.close()">EXIT</button>
 
   <svg xmlns="http://www.w3.org/2000/svg" class="icons"><symbol id="arrow-right" viewBox="0 0 1792 1792"><path d="M1600 960q0 54-37 91l-651 651q-39 37-91 37-51 0-90-37l-75-75q-38-38-38-91t38-91l293-293H245q-52 0-84.5-37.5T128 1024V896q0-53 32.5-90.5T245 768h704L656 474q-38-36-38-90t38-90l75-75q38-38 90-38 53 0 91 38l651 651q37 35 37 90z"/></symbol><symbol id="lock" viewBox="0 0 1792 1792"><path d="M640 768h512V576q0-106-75-181t-181-75-181 75-75 181v192zm832 96v576q0 40-28 68t-68 28H416q-40 0-68-28t-28-68V864q0-40 28-68t68-28h32V576q0-184 132-316t316-132 316 132 132 316v192h32q40 0 68 28t28 68z"/></symbol><symbol id="user" viewBox="0 0 1792 1792"><path d="M1600 1405q0 120-73 189.5t-194 69.5H459q-121 0-194-69.5T192 1405q0-53 3.5-103.5t14-109T236 1084t43-97.5 62-81 85.5-53.5T538 832q9 0 42 21.5t74.5 48 108 48T896 971t133.5-21.5 108-48 74.5-48 42-21.5q61 0 111.5 20t85.5 53.5 62 81 43 97.5 26.5 108.5 14 109 3.5 103.5zm-320-893q0 159-112.5 271.5T896 896 624.5 783.5 512 512t112.5-271.5T896 128t271.5 112.5T1280 512z"/></symbol></svg>
            <br> <p>Client sviluppato da Gianluca Graziadei</p>
+           
+           
+<script>
+var myWindow;
+
+function openWin() {
+  myWindow = window.open('<?php echo $link;?>', 'Classe Virtuale <?php echo $classe;?> della scuola <?php echo $scuola ?>', 'fullscreen:1');
+  
+}
+
+function closeWin() {
+  myWindow.close();
+}
+</script>
+       
 </body>
 
 <style>
+.button {
+  background-color: #ea4c88; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
 table { 
 	width: 750px; 
 	border-collapse: collapse; 
@@ -66,7 +92,8 @@ td, th {
 	padding: 10px; 
 	border: 1px solid #ccc; 
 	text-align: left; 
-	font-size: 18px;
+	font-size: 22px;
+  color: white;
 	}
 
 
